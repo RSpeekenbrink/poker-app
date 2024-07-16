6f919cc14fd7b35a59290ba9f742ee846fb75017
Remco Speekenbrink <98remco@gmail.com>
2024-07-16 14:21:12 +0100

    Fresh L11 install

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,40 +2,22 @@
 
 namespace App\Providers;
 
-use App\Contracts\ParticipantService as ParticipantServiceContract;
-use App\Contracts\RoomService as RoomServiceContract;
-use App\Services\ParticipantService;
-use App\Services\RoomService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * All of the container singletons that should be registered.
-     *
-     * @var array
-     */
-    public array $singletons = [
-        RoomServiceContract::class => RoomService::class,
-        ParticipantServiceContract::class => ParticipantService::class,
-    ];
-
-    /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         //
     }
--- resources/js/Pages/Index.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import CreateRoomForm from "@/Pages/Partials/CreateRoomForm.vue";
-</script>
-
-<template>
-  <div class="h-full">
-    <create-room-form/>
-  </div>
-</template>
